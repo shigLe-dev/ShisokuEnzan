b47a6c694d6b2ab864f68c67ea3ab42052e98822
KYapp <kukiyomenaisapp@gmail.com>
2022-12-05 00:44:47 +0900

    四則演算ができる無限桁分数構造体作った
    ただし、doubleとかfloatとかの実際の値に変換する機能はまだない

---
+++ ShisokuEnzan/EnzanPoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace ShisokuEnzan
+{
+    public struct EnzanPoint
+    {
+        public readonly BigInteger num;
+        public readonly BigInteger deno = BigInteger.One;
+
+        public EnzanPoint()
+        {
+            num = BigInteger.Zero;
+        }
+
+        public EnzanPoint(BigInteger num, BigInteger deno)
+        {
+            this.num = num;
+            this.deno = deno;
+        }
+
+        public EnzanPoint(string s)
+        {
+            num = BigInteger.Parse(s);
+        }
+
+        public static EnzanPoint operator +(EnzanPoint a, EnzanPoint b)
+        {
+            return new EnzanPoint(a.num * b.deno + b.num * a.deno, a.deno * b.deno);
+        }
+
+        public static EnzanPoint operator- (EnzanPoint a , EnzanPoint b)
+        {
+            return new EnzanPoint(a.num * b.deno - b.num * a.deno, a.deno * b.deno);
+        }
+
+        public static EnzanPoint operator* (EnzanPoint a, EnzanPoint b)
+        {
+            return new EnzanPoint(a.num * b.num, a.deno * b.deno);
+        }
+
+        public static EnzanPoint operator/ (EnzanPoint a, EnzanPoint b)
+        {
+            return new EnzanPoint(a.num * b.deno, a.deno * b.num);
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,6 @@
 ﻿using ShisokuEnzan;
 
+var e = new EnzanPoint("1") / new EnzanPoint("3") - new EnzanPoint("100");
+Console.WriteLine(e.num + " " + e.deno);
+
 Enzan.Calc("(-5) * ((100 + 100.1) * + 5) / 10 + - 23.3");
